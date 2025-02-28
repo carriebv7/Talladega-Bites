c3acf8989527b3170526746341d4ea188fcb5c5e
cvanty <107499713+cvanty@users.noreply.github.com>
2025-02-27 22:28:15 -0700

    updated weather data to group months days and include only a column for observation and date, and made plots for air temp and humidity

--- TallBitesScriptNick.R
+++ TallBitesScriptNick.R
@@ -1,6 +1,6 @@
 #Talladega Bites
 
-remotes::install_github("eco4cast/neon4cast")
+#remotes::install_github("eco4cast/neon4cast")
 library(tidyverse)
 library(neon4cast)
 library(lubridate)
@@ -18,13 +18,28 @@ tall_data <- site_data[8, ]
 ticks_data <- readr::read_csv("https://data.ecoforecast.org/neon4cast-targets/ticks/ticks-targets.csv.gz", guess_max = 1e6)
 TALL_ticks <- ticks_data[ticks_data$site_id == 'TALL',]
 
-# weather data - humidity and temperature - i'm not sure if this is what we want, however it is loading in some data - though it only goes as far back as september 2020
+# weather data - humidity and temperature - only goes as far back as september 2020
 weather_stage3 <- neon4cast::noaa_stage3()
 airtemp_hist <- as.matrix(weather_stage3 |> 
                             dplyr::filter(site_id == "TALL", variable == "air_temperature")|>
+                            dplyr::rename(ensemble = parameter) |>
+                            dplyr::select(datetime, prediction, ensemble) |>
+                            dplyr::mutate(date = as_date(datetime)) |>
+                            dplyr::group_by(date) |>
+                            dplyr::summarize(air_temperature = mean(prediction, na.rm = TRUE),
+                                             .groups = "drop") |>
+                            dplyr::rename(datetime = date) |>
+                            dplyr::mutate(air_temperature = air_temperature - 273.15) |>
                             dplyr::collect())
 humidity_hist <- as.matrix(weather_stage3 |> 
                              dplyr::filter(site_id == "TALL", variable == "relative_humidity")|>
+                             dplyr::rename(ensemble = parameter) |>
+                             dplyr::select(datetime, prediction, ensemble) |>
+                             dplyr::mutate(date = as_date(datetime)) |>
+                             dplyr::group_by(date) |>
+                             dplyr::summarize(relative_humidity = mean(prediction, na.rm = TRUE),
+                                              .groups = "drop") |>
+                             dplyr::rename(datetime = date) |>
                              dplyr::collect())
 
 ##Visualize the data
@@ -36,4 +51,28 @@ theme_set(theme_bw())
 ggplot(aes(x = datetime, y = observation), data = TALL_ticks) + 
   geom_point()
 
-  
+# converting airtemp into data frame
+airtemp <- data.frame(airtemp_hist)
+colnames(airtemp) <- c("datetime", "air_temperature")
+airtemp$air_temperature  <- as.numeric(airtemp$air_temperature)
+airtemp$datetime  <- as.Date(airtemp$datetime)
+
+
+#plotting air temp
+theme_set(theme_bw())
+ggplot(aes(x = datetime, y = air_temperature), data = airtemp) + 
+  geom_point()
+
+
+
+# converting humidity into data frame
+humidity <- data.frame(humidity_hist)
+colnames(humidity) <- c("datetime", "rel_humidity")
+humidity$rel_humidity  <- as.numeric(humidity$rel_humidity)
+humidity$datetime  <- as.Date(humidity$datetime)
+
+#plotting air temp
+theme_set(theme_bw())
+ggplot(aes(x = datetime, y = rel_humidity), data = humidity) + 
+  geom_point()
+
